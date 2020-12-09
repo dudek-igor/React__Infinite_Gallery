0b17a978de056b98f17ea19c2c5d9dd0bad30e36
Igor Dudek <dudekigor@gmail.com>
2020-12-09 16:39:42 +0100

    Update Settings

--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyledImage, StyledImageWrapper } from './Images.styled';
 import PropTypes from 'prop-types';
-// import { v4 as uuidv4 } from 'uuid';
 
 const UnsplashImages = ({ images }) => {
   return (
